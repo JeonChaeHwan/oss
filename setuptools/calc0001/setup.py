8e7c4f5e2e1801511f0b36c55da72fb3ac7078af
JeonChaeHwan <123877184+JeonChaeHwan@users.noreply.github.com>
2024-06-23 23:04:55 +0900

    Create setup.py

---
+++ setuptools/calc0001/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name = 'chaehwan',
+    version='0.0.1',
+    description='nice calculator',
+    author = 'jchwan',
+    url = 'https://github.com/JeonChaeHwan/calc0001',
+    download_url = 'https://github.com/JeonChaeHwan/calc0001/',
+    packages = ['mycalc'],
+    classifiers = [
+        'Programming Language :: Python :: 3',
+    ]
+)
